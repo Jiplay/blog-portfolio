5a17e1de36c7b8049e11bc228c02bc1d2e48d681
Julien Garsia <julien.garsia@epitech.eu>
2023-10-25 20:54:06 +0200

    EDIT : profile style

--- components/profile/ProfileText.tsx
+++ components/profile/ProfileText.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from 'react';
 import style from './Profile.module.css'
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 interface ProfileProps {
     username: string;
 }
 
 const ProfileText: React.FC<ProfileProps> = ({ username }) => {
-    return (
+  const [occupation, setOccupation] = useState<string>('');
+
+  const handleButtonClick = (value: string) => {
+    setOccupation(value);
+  };
+
+  return (
+      <div>
         <div className={style.title}>
-          <h1>{username}</h1>
-          <p>{"Bloggeur / Developper / photographe "}</p>
+          <div className={style.radio}>
+            <ButtonGroup aria-label="Basic example">
+              <Button variant="custom-button" onClick={() => handleButtonClick("Bloggeur")} className={style.customButton}>Bloggeur</Button>
+              <Button variant="custom-button" onClick={() => handleButtonClick("Developpeur")} className={style.customButton}>Developpeur</Button>
+              <Button variant="custom-button" onClick={() => handleButtonClick("Photographe")} className={style.customButton}>Photographe</Button>
+            </ButtonGroup>
+          </div>
+          <h1>{username} est {occupation}</h1>
         </div>
+          <p>{"Description catchy de fou "}</p>
+          </div>
       );
 };
 
