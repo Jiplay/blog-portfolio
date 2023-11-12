0e81701bc05fc127800150b9e333d7f98d0fa561
Julien Garsia <julien.garsia@epitech.eu>
2023-11-12 17:26:28 +0100

    1st deployment mistakes

--- components/MarkDownRenderer.tsx
+++ components/MarkDownRenderer.tsx
@@ -2,21 +2,18 @@ import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import CV from './cv/CV';
 import { UserProfileCV } from '@/backend/models/models';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 interface MarkdownRendererProps {
   source: string; // Le contenu du fichier Markdown à afficher
 }
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Column from './Column';
 
 
 const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ source }) => {
   const [markdownContent, setMarkdownContent] = useState<string>(''); // Pour stocker le contenu Markdown
 
   const profilePicture: UserProfileCV = {
-    source:"https://media.licdn.com/dms/image/D4D03AQGH-n-0wkhM7Q/profile-displayphoto-shrink_800_800/0/1680077046046?e=2147483647&v=beta&t=L-c1zdJkbYv4-ggF_CVUKNN_HbpflL6Sfd0bu9FJTvU",
+    source:"/cv/photo.jpg",
     linkedIn:"https://www.linkedin.com/in/julien-garsia/",
     github:"https://github.com/Jiplay",
     instagram:"https://www.instagram.com/jul_italiano/",
