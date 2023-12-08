1f43498f1163d6e6f112f66f0717aae09cf82628
Julien Garsia <julien.garsia@epitech.eu>
2023-12-08 10:30:20 +0100

    edit : pro index design

--- pages/pro/index.tsx
+++ pages/pro/index.tsx
@@ -66,20 +66,28 @@ export default function Index({ post, preview }: Props) {
           className="cursor-pointer"
         />
       </div>
+      <br></br>
+      <div className="flex items-center justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="rgb(75,149,230)" className="bi bi-info-circle" viewBox="0 0 16 16">
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+        </svg>
+        <h4 className="ml-2 mt-3 ml-3 ">Click on the folders to have further informations</h4>
+      </div>
+      <br></br>
         <PostBody content={post.content} />
         <Modal show={modalOpen} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
+          <Modal.Header closeButton>
           <Modal.Title>
           {ProModalContents[modalDataIterator].title !== null
           ? ProModalContents[modalDataIterator].title
           : 'Default Title'}
             </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
+          </Modal.Header>
+          <Modal.Body>
           <p>{ProModalContents[modalDataIterator].technical}</p>
           <br></br>
           <p>{ProModalContents[modalDataIterator].else}</p>
-        
         </Modal.Body>
       </Modal>
       </Container>
