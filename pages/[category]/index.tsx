3fa54f85f8279ceb7bd379e572464e264a975a93
Julien Garsia <julien.garsia@epitech.eu>
2024-03-08 09:12:49 +0100

    🎨 Add constants
    Adding subject in constant and implementation in the index

--- pages/[category]/index.tsx
+++ pages/[category]/index.tsx
@@ -10,6 +10,7 @@ import MoreStories from '../../components/more-stories';
 import Post from '../../interfaces/post'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GetServerSideProps } from 'next';
+import { categoryDescription } from '../../lib/constants';
 
 type Props = {
   allPosts: Post[]
@@ -30,8 +31,7 @@ export default function Index({ allPosts, category }: Props) {
         {category}
       </h2>
       <p className="text-lg leading-relaxed mb-4">
-        I'll write some notes about things that I already do like intermitent fasting, I'll document 
-        my thoughts and share the benefits of life not lived "by chance" but by planning.
+      {categoryDescription[category as string]}
       </p>
       <div className="flex items-center justify-center">
         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
@@ -62,82 +62,3 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     },
   };
 };
-
-// export const getStaticProps = async () => {
-//   const allPosts = getAllPosts([
-//     'title',
-//     'date',
-//     'slug',
-//     'author',
-//     'coverImage',
-//     'excerpt',
-//     'tag',
-//   ], "_posts/health")
-
-//   return {
-//     props: {
-//       allPosts,
-//     },
-//   }
-// }
-
-
-
-
-
-
-
-// import { GetServerSideProps, GetStaticProps } from 'next';
-// import { useRouter } from 'next/router';
-// import { getAllPosts } from '../../lib/api';
-
-// import Post from '../../interfaces/post'
-// import MoreStories from '../../components/more-stories';
-
-// // Si vous utilisez getServerSideProps
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const { category } = context.params;
-//   const allPosts = getAllPosts([
-//     'title',
-//     'date',
-//     'slug',
-//     'author',
-//     'coverImage',
-//     'excerpt',
-//     'tag',
-//   ], `_posts/${category}`)
-  
-  
-//   return {
-//     props: {
-//         allPosts,
-//     },
-//   };
-// };
-
-// type Props = {
-//     posts: Post[]
-// }
-
-// const CategoryPage = ({ posts }: Props) => {
-// const morePosts = posts
-//     return (
-    
-//     <div className="flex items-center justify-center">
-//         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-//       </div>
-        
-//         );
-//     };
-    
-//     export default CategoryPage;
-    
-//     {/* {posts.map((post, index) => (
-//                       <div key={index}>
-//                       <h2>{post.title}</h2>
-//                       <p>Date: {post.date}</p>
-//                       <p>Author: {post.author.name}</p>
-//                       <img src={post.coverImage} alt={post.title} />
-//                       <p>{post.excerpt}</p>
-//                   </div>
-//     ))} */}
