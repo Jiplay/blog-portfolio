3bcc4a760d74848cef918baf8071c8439a6115c0
Julien Garsia <julien.garsia@epitech.eu>
2024-06-29 10:50:41 +0200

    ✨ 404 upgrade

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,22 +4,22 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'next/image';
 
-
 const Custom404 = () => {
     return (
-        <Container className="text-center" style={{padding: '100px 0'}}>
-            <Row>
-                <Col>
-                    <h1>404 - Page Not Found</h1>
-                    <div style={{alignItems: 'center', justifyContent: 'center', display: "flex"}}>
-                        <Image src={"/assets/404.svg"}  alt={"404"} width={400} height={400}/>
+        <Container fluid className="text-center" style={{ padding: '0', height: '100vh' }}>
+            <Row className="h-100">
+                <Col className="d-flex flex-column justify-content-center align-items-center">
+                    <div style={{ position: 'relative', width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <div style={{ position: 'absolute', width: '80%', height: '80%' }}>
+                            <Image src={"/assets/404.svg"} alt={"404"} layout="fill" objectFit="contain" />
+                        </div>
                     </div>
                     <Link href="/" passHref>
-                        <Button variant="primary">Go Back Home</Button>
+                        <Button variant="primary" style={{ marginTop: '20px', zIndex: 1 }}>Go Back Home</Button>
                     </Link>
+                    <a href="https://storyset.com/online" style={{ marginTop: '20px', zIndex: 1 }}>Online illustrations by Storyset</a>
                 </Col>
             </Row>
-            <a href="https://storyset.com/online">Online illustrations by Storyset</a>
         </Container>
     );
 };
