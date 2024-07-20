d5dc6ca0af14811180a7cf1a6906c57ac9be6b22
Julien Garsia <julien.garsia@epitech.eu>
2024-07-20 16:13:41 +0200

    teste

--- pages/[category]/[slug].tsx
+++ pages/[category]/[slug].tsx
@@ -39,7 +39,7 @@ export default function Post({ post, preview }: Props) {
               <article className="mb-32">
                 <Head>
                   <title>{title}</title>
-                  <meta property="og:image" content={post.ogImage.url} />
+                  {/*<meta property="og:image" content={post.ogImage.url} />*/}
                 </Head>
                 <PostHeader
                   title={post.title}
